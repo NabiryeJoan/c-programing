d5fb6f38a6f7e0bd10c77caac6516e588e0e21c1
NabiryeJoan <nabiryejoan361@gmail.com>
2023-07-07 11:01:24 +0300

    while loop

--- 1st/while.py
+++ 1st/while.py
@@ -19,3 +19,29 @@ int main() {
 
     return 0;
 }
+
+
+
+#example 2
+# we have a while loop that runs until the number reaches 11.
+#  Inside the loop, we use a conditional statement (if-else)
+#  to check if the number is even or odd. Based on the result
+#include <stdio.h>
+
+int main() {
+    int number = 1;
+
+    while (number <= 10) {
+        if (number % 2 == 0) {
+            printf("%d is even.\n", number);
+        } else {
+            printf("%d is odd.\n", number);
+        }
+        
+        number++;
+    }
+
+    printf("Loop finished.\n");
+
+    return 0;
+}
